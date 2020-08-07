e2525907de20eee42ae62decd3386da21cda408c
Sakib Alamin <sakibalamin162@gmail.com>
2020-08-07 14:29:09 +0600

    JSX #8

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       {/* same as above, with a self closing as there's nothing inside it */}
       <Greet />
       <Welcome />
+      <Hello />
     </div>
   );
 }
