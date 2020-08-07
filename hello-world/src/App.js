af8606826b5fbb07417b5dc3ea4dd0966f8c7c6f
Sakib Alamin <sakibalamin162@gmail.com>
2020-08-07 13:22:38 +0600

    functional components #5

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet> </Greet> */}
+      {/* same as above, with a self closing as there's nothing inside it */}
+      <Greet />
     </div>
   );
 }
---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// normal function
+// function Greet() {
+//     return <h1> Hello Sakib! </h1>
+// }
+
+// using arrow function
+// const Greet = () => {
+//     return <h1> Hello Sakib!! </h1>
+// };
+
+// same thing as above
+
+const Greet = () => <h1> Hello Sakib! </h1>
+
+// exporting as default enables us to import it by any name
+// other one is constant
+export default Greet;
